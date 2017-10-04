2e39c0f12be9747066f307779c01b314efbe624c
Daniel Sørensen <1058493@ucn.dk>
2017-10-04 16:17:24 +0200

    first commit

---
+++ EasyTime/EasyTime/EasyTime/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EasyTime
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            listview.ItemsSource = "";
+            listview4.ItemsSource = "";
+            listview5.ItemsSource = "";
+        }
+    }
+}
