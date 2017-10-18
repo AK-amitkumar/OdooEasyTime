e211d3df4f337cc763cc65076f0dbbd94271df0d
Daniel070793 <daniel070793@gmail.com>
2017-10-18 13:23:37 +0200

    added some new features

---
+++ EasyTimeOdoo/EasyTimeOdoo/Model/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTimeOdoo.Model
+{
+    class Activity
+    {
+        public string Title { get; set; }
+        public int TaskID { get; set; }
+        public string Description { get; set; }
+        public int ProjectID { get; set; }
+        public string UserID { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string timeElapsed { get; set; }
+    }
+}
