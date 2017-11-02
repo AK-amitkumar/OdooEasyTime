e9f7d03fdb3972c32daa284df40421368357e511
RamananSri <1058515@ucn.dk>
2017-11-02 09:23:25 +0100

    old code

--- EasyTimeOdoo/EasyTimeOdoo/DrivingPage.xaml.cs
+++ EasyTimeOdoo/EasyTimeOdoo/DrivingPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EasyTimeOdoo.Modal;
+using EasyTimeOdoo.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,20 @@ namespace EasyTimeOdoo
         public DrivingPage()
         {
             InitializeComponent();
+            listview.ItemsSource = _activeDrive;
         }
+
+        public ObservableCollection<ActivityDriving> _activeDrive = new ObservableCollection<ActivityDriving>
+            {
+                new ActivityDriving{Title = "Odoo House", Description = "Møde", Distance = "14", sDate = "10-10-0217"},
+                new ActivityDriving{Title = "Odoo House", Description = "Mail opsætning", Distance = "7", sDate = "12-10-0217"}
+            };
+
+        async void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            var page = new NewActivityDriveModal(_activeDrive);
+            await Navigation.PushModalAsync(page);
+        }
+
     }
 }
